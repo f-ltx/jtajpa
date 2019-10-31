1943f09ed4923a9e5da09560e6fdeb969c5a94f1
litianxiang <litianxiang@topnet.net.cn>
2019-10-31 11:31:23 +0800

    加入druid展示

--- src/main/java/cn/ltx/jtajpa/JtajpaApplication.java
+++ src/main/java/cn/ltx/jtajpa/JtajpaApplication.java
@@ -5,6 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * Description:在springbootApplication上加上"@ServletComponentScan"注解后，
+ * Servlet、Filter、Listener可以直接通过
+ * "@WebServlet"、"@WebFilter"、"@WebListener"注解自动注册，无需其他代码
+ *
+ * @author litianxiang
+ * @date 2019-10-31
+ */
 @SpringBootApplication
 @ServletComponentScan
 public class JtajpaApplication {
